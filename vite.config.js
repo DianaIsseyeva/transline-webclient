72dca00f88cfef98fb0e8f93c95a5019cd68e9f1
Diana <diana.dev777@gmail.com>
2025-10-29 18:02:14 +0500

    refactor: config vite

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,8 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-// import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    // svgr({
-    //   include: '**/*.svg',
-    //   exportAsDefault: true,
-    // }),
-  ],
+  plugins: [react(), tailwindcss()],
   resolve: { alias: { '@': '/src' } },
 });
